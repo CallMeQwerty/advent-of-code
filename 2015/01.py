73fd80af75f2f5571e27c94ef2d17d34e3be6804
Call Me Qwerty <bankowski.play@gmail.com>
2023-07-15 23:27:45 +0200

    modified:   2015/01.py

--- 2015/01.py
+++ 2015/01.py
@@ -1,21 +1,17 @@
-
 file_path = "2015/inputs/01.txt"
-file = open(file_path, "r")
 
-file_contents = file.read()
+with open(file_path, "r") as file:
+    file_contents = file.read()
 
 floor = 0
 position = 0
 found_basement = False
 
-for item in file_contents:
-    if item == '(': floor += 1
-    if item == ')': floor -= 1
+for char in file_contents:
+    floor += 1 if char == '(' else -1 if char == ')' else 0
 
-    if found_basement == False: position += 1
-    if floor < 0 and found_basement == False: found_basement = True
-  
-file.close()
+    if not found_basement: position += 1
+    if floor < 0 and not found_basement: found_basement = True
 
 print(floor)
 print(position)
